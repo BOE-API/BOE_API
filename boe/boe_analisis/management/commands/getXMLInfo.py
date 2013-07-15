459168bbd451ee39c6ab4d5c72d7de1b55b1c8ba
Carlos Simón Villas <csimonts@gmail.com>
2013-07-15 07:07:57 +0200

    try catch errors to file

--- boe/boe_analisis/management/commands/getXMLInfo.py
+++ boe/boe_analisis/management/commands/getXMLInfo.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
        print 'Probando mongo'
 
 r = redis.StrictRedis(host='23.23.215.173', port=6379, db=0)
-
+f = file('xml_failed.txt', 'w+')
 c = int(r.get('counter_xml'))
 max = Documento.objects.count()
 while c < max:
@@ -36,64 +36,68 @@ while c < max:
             estado = etree.XPath("//estado_consolidacion")
             ant = etree.XPath("//anterior")
             post = etree.XPath("//posterior")
-            for an in ant(root):
+            try:
 
-                anterior = objectify.fromstring(etree.tostring(an))
-                referencia = anterior.get('referencia')
-                doc_ref = Documento(identificador=referencia)
-                doc_ref.save()
-                palabra_codigo = anterior.palabra.get('codigo')
-                palabra_texto = anterior.palabra.text
-                texto = anterior.texto.text
-                print doc_ref.identificador
-                palabra = Palabra(codigo = palabra_codigo, titulo=palabra_texto)
-                palabra.save()
-                ref = Referencia(referencia=doc_ref, palabra=palabra, texto=texto)
-                try:
-                    ref.save()
-                except:
+                for an in ant(root):
+
+                    anterior = objectify.fromstring(etree.tostring(an))
+                    referencia = anterior.get('referencia')
+                    doc_ref = Documento(identificador=referencia)
+                    doc_ref.save()
+                    palabra_codigo = anterior.palabra.get('codigo')
+                    palabra_texto = anterior.palabra.text
+                    texto = anterior.texto.text
+                    print doc_ref.identificador
+                    palabra = Palabra(codigo = palabra_codigo, titulo=palabra_texto)
+                    palabra.save()
+                    ref = Referencia(referencia=doc_ref, palabra=palabra, texto=texto)
                     try:
-                        ref = Referencia.objects.get(referencia=doc_ref, palabra=palabra)
+                        ref.save()
                     except:
-                        pass
+                        try:
+                            ref = Referencia.objects.get(referencia=doc_ref, palabra=palabra)
+                        except:
+                            pass
 
-                refAnteriores.append(ref)
+                    refAnteriores.append(ref)
 
-            for an in post(root):
-                anterior = objectify.fromstring(etree.tostring(an))
-                referencia = anterior.get('referencia')
-                doc_ref = Documento(identificador=referencia)
-                doc_ref.save()
-                palabra_codigo = anterior.palabra.get('codigo')
-                palabra_texto = anterior.palabra.text
-                texto = anterior.texto.text
-                palabra = Palabra(codigo = palabra_codigo, titulo=palabra_texto)
-                palabra.save()
-                ref = Referencia(referencia=doc_ref, palabra=palabra, texto=texto)
-                try:
-                    ref.save()
-                except:
+                for an in post(root):
+                    anterior = objectify.fromstring(etree.tostring(an))
+                    referencia = anterior.get('referencia')
+                    doc_ref = Documento(identificador=referencia)
+                    doc_ref.save()
+                    palabra_codigo = anterior.palabra.get('codigo')
+                    palabra_texto = anterior.palabra.text
+                    texto = anterior.texto.text
+                    palabra = Palabra(codigo = palabra_codigo, titulo=palabra_texto)
+                    palabra.save()
+                    ref = Referencia(referencia=doc_ref, palabra=palabra, texto=texto)
                     try:
-                        ref = Referencia.objects.get(referencia=doc_ref, palabra=palabra)
+                        ref.save()
                     except:
-                        pass
-                refPosteriores.append(ref)
+                        try:
+                            ref = Referencia.objects.get(referencia=doc_ref, palabra=palabra)
+                        except:
+                            pass
+                    refPosteriores.append(ref)
 
-            for ori in origen(root):
-                origen_codigo = ori.get('codigo')
-                origen_texto = ori.text
-                orig = Origen_legislativo(codigo=int(origen_codigo), titulo=origen_texto)
-                orig.save()
-                doc.origen_legislativo = orig
-            for est in estado(root):
-                estado_codigo = est.get('codigo')
-                if (estado_codigo != ''):
-                    estado_texto = est.text
-                    estado = Estado_consolidacion(codigo=int(estado_codigo), titulo=estado_texto)
-                    estado.save()
+                for ori in origen(root):
+                    origen_codigo = ori.get('codigo')
+                    origen_texto = ori.text
+                    orig = Origen_legislativo(codigo=int(origen_codigo), titulo=origen_texto)
+                    orig.save()
+                    doc.origen_legislativo = orig
+                for est in estado(root):
+                    estado_codigo = est.get('codigo')
+                    if (estado_codigo != ''):
+                        estado_texto = est.text
+                        estado = Estado_consolidacion(codigo=int(estado_codigo), titulo=estado_texto)
+                        estado.save()
 
-            doc.referencias_anteriores = refAnteriores
-            doc.referencias_posteriores = refPosteriores
-            doc.save()
+                doc.referencias_anteriores = refAnteriores
+                doc.referencias_posteriores = refPosteriores
+                doc.save()
+            except:
+                f.write(doc.identificador)
 
     c = int(r.get('counter_xml'))
