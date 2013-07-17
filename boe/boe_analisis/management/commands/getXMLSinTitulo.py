dbdc965ba57491361c1917f03ea01b9f32159b2e
Carlos Simón Villas <csimonts@gmail.com>
2013-07-17 14:04:59 +0200

    redis XML INFO

--- boe/boe_analisis/management/commands/getXMLSinTitulo.py
+++ boe/boe_analisis/management/commands/getXMLSinTitulo.py
@@ -27,29 +27,46 @@ url_a_html_pattern = "http://www.boe.es/diario_boe/txt.php?id={0}"
 
 r_count = redis.StrictRedis(host='23.23.215.173', port=6379, db=0)
 r = redis.StrictRedis(host='50.17.220.245', port=6379, db=0)
-max = 100000
-count = int(r_count.get('count_empty'));
 
 
+if (len(sys.argv) >= 3):
+    rango =  sys.argv[2]
+
+else:
+    print 'manda un argumento'
+    sys.exit()
 
 
+max = 300
+count = 0
+test_ue = r.lrange(rango, count, count+100)
 
+# print test_ue
+print test_ue
 
-
-print count
-test = Documento.objects.filter(titulo=None).exclude(
-    Q(identificador__istartswith='--') |
-    Q(identificador__istartswith='B-B') |
-    Q(identificador__istartswith='BOA'))
-
-while int(count) < max:
-    r_count.incr('count_empty', amount=10)
-    for e in test[count:count+10]:
-        url = url_a_pattern.format(e.identificador)
+while count < max:
+    test_ue = r.lrange(rango, count, count+100)
+    for url in test_ue:
+        documento = Documento()
+        fillDocumentXMLData(url, documento)
         print url
-        fillDocumentXMLData(url, e)
+    count += 100
+    print count
 
-    count = int(r_count.get('count_empty'));
 
+
+
+
+# print count
+#
+# while int(count) < max:
+#     r_count.incr('count_empty', amount=10)
+#     for e in test[count:count+10]:
+#         url = url_a_pattern.format(e.identificador)
+#         print url
+#         fillDocumentXMLData(url, e)
+#
+#     count = int(r_count.get('count_empty'));
+#
 
 
