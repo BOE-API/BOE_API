a902e2db1e1a50e095c9bc3d896abe4476f993a1
Carlos Simón Villas <csimonts@gmail.com>
2013-07-21 22:13:52 +0200

    field searchindexes

--- boe/boe_analisis/search_indexes.py
+++ boe/boe_analisis/search_indexes.py
@@ -6,8 +6,8 @@ class JobIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True, model_attr='texto')
     titulo = indexes.CharField(model_attr='titulo', null=True)
     fecha_publicacion = indexes.DateField(model_attr='fecha_publicacion', null=True)
-    departamento = indexes.CharField(model_attr='departamento_titulo', null=True)
-    materias = indexes.CharField(model_attr='materias_titulo', null=True)
+    departamento = indexes.CharField(model_attr='departamento__titulo', null=True)
+    materias = indexes.CharField(model_attr='materias__titulo', null=True)
 
     def get_model(self):
         return Documento
