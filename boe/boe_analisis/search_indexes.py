766cc930144baaaf14bacfe51abcb363fe9a2fd8
Carlos Simón Villas <csimonts@gmail.com>
2013-07-21 22:05:26 +0200

    search_indexes

--- boe/boe_analisis/search_indexes.py
+++ boe/boe_analisis/search_indexes.py
@@ -2,11 +2,12 @@ from haystack import indexes
 from boe_analisis.models import Documento
 from boe_analisis.models import *
 class JobIndex(indexes.SearchIndex, indexes.Indexable):
+    identificador = indexes.CharField(model_attr='identificador', null=True)
     text = indexes.CharField(document=True, use_template=True, model_attr='texto')
-    titulo = indexes.CharField(model_attr='titulo')
-    fecha_publicacion = indexes.DateField(model_attr='fecha_publicacion')
-    departamento = indexes.CharField(model_attr='departamento_titulo')
-    materias = indexes.CharField(model_attr='materias_titulo', )
+    titulo = indexes.CharField(model_attr='titulo', null=True)
+    fecha_publicacion = indexes.DateField(model_attr='fecha_publicacion', null=True)
+    departamento = indexes.CharField(model_attr='departamento_titulo', null=True)
+    materias = indexes.CharField(model_attr='materias_titulo', null=True)
 
     def get_model(self):
         return Documento
