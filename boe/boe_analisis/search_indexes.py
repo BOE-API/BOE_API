f16ffc6cced8a81ff187366cc1673751d1fdbaab
Carlos Simón Villas <csimonts@gmail.com>
2013-07-21 23:46:41 +0200

    debugger

--- boe/boe_analisis/search_indexes.py
+++ boe/boe_analisis/search_indexes.py
@@ -1,16 +1,26 @@
 from haystack import indexes
 from boe_analisis.models import Documento
 from boe_analisis.models import *
-class JobIndex(indexes.SearchIndex, indexes.Indexable):
-    identificador = indexes.CharField(model_attr='identificador', null=True)
-    text = indexes.CharField(document=True, use_template=True, model_attr='texto')
-    titulo = indexes.CharField(model_attr='titulo', null=True)
-    fecha_publicacion = indexes.DateField(model_attr='fecha_publicacion', null=True)
-    departamento = indexes.CharField(model_attr='departamento__titulo', null=True)
-    materias = indexes.CharField(model_attr='materias__titulo', null=True)
+import pdb
 
-    def get_model(self):
-        return Documento
- 
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
+pdb.set_trace()
+# class JobIndex(indexes.SearchIndex, indexes.Indexable):
+#     identificador = indexes.CharField(model_attr='identificador', null=True)
+#     text = indexes.CharField(document=True, use_template=True, model_attr='texto')
+#     titulo = indexes.CharField(model_attr='titulo', null=True)
+#     fecha_publicacion = indexes.DateField(model_attr='fecha_publicacion', null=True)
+#     # departamento = indexes.CharField(model_attr='departamento__titulo', null=True)
+#     # materias = indexes.CharField(model_attr='materias__titulo', null=True)
+#
+#     def get_model(self):
+#         return Documento
+#
+#     def index_queryset(self, using=None):
+#         return self.get_model().objects.all()
+
+import datetime
+
+# All Fields
+class AllNoteIndex(indexes.ModelSearchIndex, indexes.Indexable):
+    class Meta:
+        model = Documento
