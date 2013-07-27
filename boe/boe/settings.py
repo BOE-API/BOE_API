ff9101dde9ce56080ebca726fdb9465819800198
Carlos Simón Villas <csimonts@gmail.com>
2013-07-27 12:29:00 +0200

    new url for static content

--- boe/boe/settings.py
+++ boe/boe/settings.py
@@ -61,20 +61,20 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'http://cuantocobran.es/static/'
+STATIC_ROOT = 'http://cuantocobran.es:81/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'http://cuantocobran.es/static/'
+STATIC_URL = 'http://cuantocobran.es:81/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('img', 'http://cuantocobran.es/static/img/'),
-    ('css', 'http://cuantocobran.es/static/css/'),
-    ('js', 'http://cuantocobran.es/static/js/'),
+    ('img', 'http://cuantocobran.es:81/static/img/'),
+    ('css', 'http://cuantocobran.es:81/static/css/'),
+    ('js', 'http://cuantocobran.es:81/static/js/'),
 
 
 )
