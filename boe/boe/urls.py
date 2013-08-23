274fef0530ebf650ca99c47bf2abead92b26f5df
Carlos Simón Villas <csimonts@gmail.com>
2013-08-23 23:30:38 +0200

    remove social networks dependencies

--- boe/boe/urls.py
+++ boe/boe/urls.py
@@ -2,7 +2,7 @@ from boe_analisis.models import Documento, Diario, Partido, Legislatura
 from boe_analisis.urls import *
 from django.conf.urls import patterns, include, url
 from boe_analisis import views
-from social_networks.models import *
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 #
@@ -10,10 +10,7 @@ admin.autodiscover()
 #admin.site.register(Documento)
 #admin.site.register(Partido)
 #admin.site.register(Legislatura)
-admin.site.register(TwitterAccount)
-admin.site.register(Word)
-admin.site.register(FacebookAccount)
-admin.site.register(Subject)
+
 
 
 
@@ -28,6 +25,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     # url(r'^search/', include('haystack.urls')),
 )
